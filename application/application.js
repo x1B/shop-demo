d3ccacbabc3128780bd1baea248ee49c4fef54e6
Alexander Wilden <alexander.wilden@aixigo.de>
2016-08-03 11:00:17 +0200

    Using new page.js features and config

--- application/application.js
+++ application/application.js
@@ -2,15 +2,23 @@
 window.laxarConfig = ( function() {
    'use strict';
 
+   /* eslint-disable no-var,prefer-template */
    var modeAttribute = 'data-ax-application-mode';
    var mode = document.querySelector( 'script[' + modeAttribute + ']' ).getAttribute( modeAttribute );
 
    return {
+      baseHref: window.location.pathname,
       tooling: {
          enabled: true
       },
       logging: {
          threshold: 'TRACE'
+      },
+      flow: {
+         router: {
+            hashbang: true,
+            dispatch: true
+         }
       },
       name: 'LaxarJS ShopDemo',
       description: 'A DemoApp to learn how LaxarJS works.',
