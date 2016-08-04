45706a0b4221654e251adef93fc1f627ebb7dbc7
Michael Kurze <michael.kurze@aixigo.de>
2016-08-04 09:19:55 +0200

    fixed laxar loader arguments

--- init.js
+++ init.js
@@ -10,7 +10,7 @@ import { bootstrap } from 'laxar';
 import * as angularAdapter from 'laxar-angular-adapter';
 import * as reactAdapter from 'laxar-react-adapter';
 
-import artifacts from 'laxar-loader?flows=main&themes=cube,default!./package.json';
+import artifacts from 'laxar-loader?flows=main&themes[]=default&themes[]=cube!./package.json';
 
 bootstrap( document.querySelector( '[data-ax-page]' ), {
    widgetAdapters: [ angularAdapter, reactAdapter ],
