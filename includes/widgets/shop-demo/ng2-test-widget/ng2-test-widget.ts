ab340a1f08aab9b313a9e75d52c223495e0b057c
Alexander Wilden <alexander.wilden@aixigo.de>
2017-01-05 17:34:21 +0100

    Added themed assets to widget

--- includes/widgets/shop-demo/ng2-test-widget/ng2-test-widget.ts
+++ includes/widgets/shop-demo/ng2-test-widget/ng2-test-widget.ts
@@ -1,54 +1,29 @@
-import { Component, NgModule, OnDestroy } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { AxEventBus, AxContext } from 'laxar-types';
 import { AxAngularModule } from 'laxar-angular2-adapter';
 
 @Component( {
-   template: `
-      <h1>Hello {{name}}</h1>
-      <div [axWidgetArea]="areaName"></div>
-      <input (input)="nameChanged($event.target.value)" value="{{name}}">
-   `
+   templateUrl: 'ax-widget:template:ng2-test-widget'
 } )
-export class Ng2TestWidget implements OnDestroy {
+export class Ng2TestWidget {
 
    name = 'World';
    areaName: any;
 
    constructor( eventBus: AxEventBus, context: AxContext ) {
       this.areaName = context.features.areaName;
-
-      eventBus.subscribe( 'didNavigate', (event, meta) => {
-         this.name = meta.cycleId;
-         console.log( 'didNavigate' );
-      } );
-      eventBus.subscribe( 'beginLifecycleRequest', (event, meta) => {
-         this.name = meta.cycleId;
-         console.log( 'beginLifecycleRequest' );
-      } );
-      eventBus.subscribe( 'didReplace', (event, meta) => {
-         this.name = meta.cycleId;
-         console.log( 'didReplace', meta.cycleId );
-      } );
    }
 
    nameChanged( newName: string ): void {
-      console.log( newName );
       this.name = newName;
-   }
-
-   ngOnInit(): void {
-      console.log( 'hello there' );
-   }
-
-   ngOnDestroy(): void {
-      console.log( 'goodbye widget!' );
    }
 
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 @NgModule( {
    imports: [ AxAngularModule ],
    declarations: [ Ng2TestWidget ]
 } )
-export class Ng2TestWidgetModule { }
+export class Ng2TestWidgetModule {}
